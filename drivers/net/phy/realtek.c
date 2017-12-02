e017b70540d7144d3114fa92ddf0769ec5165e53
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2017-12-02 23:06:48 +0100

    net: phy: realtek: add support for configuring the RX delay on RTL8211F
    On RTL8211F the RX delay can also be enabled/disabled.
    The overall behavior of the RX delay is similar to the behavior of the
    TX delay, which was already supported by the driver.
    
    The RX delay (similar to the TX delay) may be enabled using hardware pin
    strapping. If the MAC already configures the RX delay (if required) then
    the RX delay generated by the RTL8211F PHY has to be turned off.
    
    While here, update the comment regarding the TX delay why it has to be
    enabled or disabled within the driver.
    Also avoid code-duplication by extracting the code to mask/unmask bits
    in a paged register into a new rtl8211x_page_mask_bits helper function.
    
    Signed-off-by: Martin Blumenstingl <martin.blumenstingl@googlemail.com>

--- drivers/net/phy/realtek.c
+++ drivers/net/phy/realtek.c
@@ -32,7 +32,10 @@
 
 #define RTL8211F_INSR				0x1d
 
-#define RTL8211F_TX_DELAY			BIT(8)
+#define RTL8211F_RX_DELAY_REG			0x15
+#define RTL8211F_RX_DELAY_EN			BIT(3)
+#define RTL8211F_TX_DELAY_REG			0x11
+#define RTL8211F_TX_DELAY_EN			BIT(8)
 
 #define RTL8201F_ISR				0x1e
 #define RTL8201F_IER				0x13
@@ -137,12 +140,39 @@ static int rtl8211f_config_init(struct phy_device *phydev)
 	if (ret < 0)
 		return ret;
 
-	/* enable TX-delay for rgmii-id and rgmii-txid, otherwise disable it */
+	/*
+	 * enable TX-delay for rgmii-id and rgmii-txid, otherwise disable it.
+	 * this is needed because it can be enabled by pin strapping and
+	 * conflict with the TX-delay configured by the MAC.
+	 */
 	if (phydev->interface == PHY_INTERFACE_MODE_RGMII_ID ||
 	    phydev->interface == PHY_INTERFACE_MODE_RGMII_TXID)
-		val = RTL8211F_TX_DELAY;
+		val = RTL8211F_TX_DELAY_EN;
+	else
+		val = 0;
 
-	return phy_modify_paged(phydev, 0xd08, 0x11, RTL8211F_TX_DELAY, val);
+	ret = phy_modify_paged(phydev, 0xd08, RTL8211F_TX_DELAY_REG,
+				      RTL8211F_TX_DELAY_EN, val);
+	if (ret)
+		return ret;
+
+	/*
+	 * enable RX-delay for rgmii-id and rgmii-rxid, otherwise disable it.
+	 * this is needed because it can be enabled by pin strapping and
+	 * conflict with the RX-delay configured by the MAC.
+	 */
+	if (phydev->interface == PHY_INTERFACE_MODE_RGMII_ID ||
+	    phydev->interface == PHY_INTERFACE_MODE_RGMII_RXID)
+		val = RTL8211F_RX_DELAY_EN;
+	else
+		val = 0;
+
+	ret = phy_modify_paged(phydev, 0xd08, RTL8211F_RX_DELAY_REG,
+				      RTL8211F_RX_DELAY_EN, val);
+	if (ret)
+		return ret;
+
+	return 0;
 }
 
 static struct phy_driver realtek_drvs[] = {
