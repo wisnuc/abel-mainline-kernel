98e0cfdcef2508b690cb70c27963eea048cee930
Kamil Trzciński <ayufan@ayufan.eu>
2018-05-29 08:25:29 +0200

    ayufan: ci: update dev scripts
    Change-Id: I1e5f992e944e3de8466ebffd9ce182e92895ba1e

--- dev.mk
+++ dev.mk
@@ -1,13 +1,16 @@
-RELEASE ?= $(shell date +%Y%m%d%H%M)
-KERNEL_EXTRAVERSION ?= -rockchip-ayufan
+RELEASE ?= 1~dev
+KERNEL_EXTRAVERSION ?= -rockchip-ayufan-$(RELEASE)
 KERNEL_DEFCONFIG ?= rockchip_linux_defconfig
+
+BOARD ?= rk3328-rock64
+
+export KDEB_PKGVERSION=$(RELEASE)~ayufan
 
 KERNEL_MAKE ?= make \
 	EXTRAVERSION=$(KERNEL_EXTRAVERSION) \
-	KDEB_PKGVERSION=$(RELEASE) \
 	ARCH=arm64 \
 	HOSTCC=aarch64-linux-gnu-gcc \
-	CROSS_COMPILE="ccache aarch64-linux-gnu-" \
+	CROSS_COMPILE="ccache aarch64-linux-gnu-"
 
 KERNEL_RELEASE ?= $(shell $(KERNEL_MAKE) -s kernelversion)
 
@@ -23,17 +26,17 @@ info: .config
 
 .PHONY: kernel-menuconfig
 kernel-menuconfig:
-	$(KERNEL_MAKE) $(DEFCONFIG)
+	$(KERNEL_MAKE) $(KERNEL_DEFCONFIG)
 	$(KERNEL_MAKE) HOSTCC=gcc menuconfig
 	$(KERNEL_MAKE) savedefconfig
 	mv defconfig arch/arm64/configs/$(KERNEL_DEFCONFIG)
 
 .PHONY: kernel-image
-kernel-image:
+kernel-image: .config
 	$(KERNEL_MAKE) Image dtbs -j$$(nproc)
 
 .PHONY: kernel-modules
-kernel-image-and-modules:
+kernel-image-and-modules: .config
 	$(KERNEL_MAKE) Image modules dtbs -j$$(nproc)
 	$(KERNEL_MAKE) modules_install INSTALL_MOD_PATH=$(CURDIR)/out/linux_modules
 
@@ -42,12 +45,12 @@ kernel-package: .config
 	$(KERNEL_MAKE) bindeb-pkg -j$$(nproc)
 
 .PHONY: kernel-update-dts
-kernel-update-dts:
+kernel-update-dts: .config
 	$(KERNEL_MAKE) dtbs -j$$(nproc)
-	rsync --partial --checksum -rv arch/arm64/boot/dts/rockchip/rk3328-rock64.dtb root@$(REMOTE_HOST):$(REMOTE_DIR)/boot/efi/dtb
+	rsync --partial --checksum -rv arch/arm64/boot/dts/rockchip/$(BOARD).dtb root@$(REMOTE_HOST):$(REMOTE_DIR)/boot/efi/dtb
 
 .PHONY: kernel-update
 kernel-update-image:
 	rsync --partial --checksum -rv arch/arm64/boot/Image root@$(REMOTE_HOST):$(REMOTE_DIR)/boot/efi/Image
-	rsync --partial --checksum -rv arch/arm64/boot/dts/rockchip/rk3328-rock64.dtb root@$(REMOTE_HOST):$(REMOTE_DIR)/boot/efi/dtb
+	rsync --partial --checksum -rv arch/arm64/boot/dts/rockchip/$(BOARD).dtb root@$(REMOTE_HOST):$(REMOTE_DIR)/boot/efi/dtb
 	rsync --partial --checksum -av out/linux_modules/lib/ root@$(REMOTE_HOST):$(REMOTE_DIR)/lib
